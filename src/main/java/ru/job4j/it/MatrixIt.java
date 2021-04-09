142565a73a0ee8355b636d45c83a525bb563707c
serj <sergeykalenikov@gmail.com>
2021-04-09 11:16:04 +0300

    5.1.1. Итератор для двухмерного массива int[][][9539#513721]

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -1,6 +1,5 @@
 package ru.job4j.it;
 
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -17,7 +16,9 @@ public class MatrixIt implements Iterator<Integer> {
 //        int[][] array = new int[][]{{1}, {2}, {3}};
 //        int[][] array = new int[][]{{11, 12}, {}, {31}, {}, {51}};
 //        int[][] array = new int[][]{{1, 2}, {3}};
-        int[][] array = new int[][]{{}, {11}};
+//        int[][] array = new int[][]{{}, {}};
+//        int[][] array = new int[][]{{}, {21}};
+        int[][] array = new int[][]{{}, {}, {31}, {}, {}, {61}};
         MatrixIt i = new MatrixIt(array);
         while (i.hasNext()) {
             System.out.println(i.next());
@@ -30,18 +31,11 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        // здесь мы должны понять, есть ли дальше (начиная с текущей позиции) элементы в матрице
-        // для оптимизации можно сразу передвинуть указатели, чтобы не повторять поиск дальше в методе next()
-        // (например для случая, если в матрице много пустых строк)
-
-        for (int row = this.row; row < data.length; row++) {
-            for (int column = this.column; column < data[row].length; column++) {
-                this.row = row;
-                this.column = column;
-                return true;
-            }
+        while (row < data.length && data[row].length == 0) {
+            row++;
+            column = 0;
         }
-        return false;
+        return row < data.length;
     }
 
     @Override
@@ -49,16 +43,10 @@ public class MatrixIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        // здесь мы должны вернуть элемент с позиции текущих указателей (которые определили в hasNext)
-        // и передвинуть их на шаг вперед
         int next = data[row][column];
-        System.out.print("строка " + row + ", колонок в строке " + data[row].length + ", значение ячейки: ");
         column++;
-        // если стоим на последней колонке строки
         if (column == data[row].length) {
-            // переходим в 1ю колонку
             column = 0;
-            // новой строки
             row++;
         }
         return next;
