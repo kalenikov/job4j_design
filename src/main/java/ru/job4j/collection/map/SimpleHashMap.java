80b52b030fe2ad624f7787e9d34ea403d17d329b
serj <sergeykalenikov@gmail.com>
2021-04-19 12:23:24 +0300

    8. Реализовать собственную структуру данных - HashMap [#513745]

---
+++ src/main/java/ru/job4j/collection/map/SimpleHashMap.java
@@ -0,0 +1,155 @@
+package ru.job4j.collection.map;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleHashMap<K, V> implements Iterable<V> {
+    private static int DEFAULT_CAPACITY = 16;
+    private static float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Node<K, V>[] table;
+    private int size;
+    private int modCount;
+    private int threshold;
+
+    public SimpleHashMap(int capacity) {
+        table = new Node[capacity];
+        // когда кол-во добавленных элементов достигнет этого значения, размер таблицы будет увеличен
+        threshold = (int) (capacity * DEFAULT_LOAD_FACTOR);
+    }
+
+    public SimpleHashMap() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    private static class Node<K, V> {
+        K key;
+        V value;
+        int hash;
+
+        public Node(K key, V value, int hash) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%s, %s, %d)", key, value, hash);
+        }
+    }
+
+    private int hash(K key) {
+        if (key == null) {
+            return 0;
+        }
+        int hk = key.hashCode();
+        return hk ^ (hk >>> 16);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(table);
+    }
+
+    protected void resize() {
+        if (size > threshold) {
+            int newCapacity = table.length * 2;
+            this.threshold = (int) (newCapacity * DEFAULT_LOAD_FACTOR);
+            Node<K, V> row;
+            Node<K, V>[] newTable = new Node[newCapacity];
+            System.out.println(String.format("resize to %d, new threshold: %d", newCapacity, threshold));
+            for (int i = 0; i < table.length; i++) {
+                if ((row = table[i]) == null) {
+                    continue;
+                }
+                int idx = row.hash & (newCapacity - 1);
+                newTable[idx] = row;
+            }
+            table = newTable;
+        }
+    }
+
+    public boolean insert(K key, V value) {
+        Node<K, V> row;
+        int hash;
+        int i = (hash = hash(key)) & (table.length - 1);
+        // если в этой ячейке еще ничего нет, добавляем туда элемент
+        if ((row = table[i]) == null) {
+            table[i] = new Node<>(key, value, hash);
+            size++;
+            modCount++;
+            resize();
+            return true;
+        } else {
+            // если в эту ячейку уже что-то записано
+            // и ключи совпадают
+            if (row.hash == hash
+                    && row.key.equals(key)) {
+                table[i].value = value;
+                modCount++;
+                return true;
+            } else {
+                // ключи не совпадают (произошла коллизия)
+                return false;
+            }
+        }
+    }
+
+    public V get(K key) {
+        int hash = hash(key);
+        Node<K, V> node = table[hash & (table.length - 1)];
+        return node == null ? null : node.value;
+    }
+
+    public boolean delete(K key) {
+        int hash = hash(key);
+        Node<K, V> node = table[hash & (table.length - 1)];
+        table[hash & (table.length - 1)] = null;
+        size--;
+        modCount++;
+        return node != null;
+    }
+
+
+    @Override
+    public Iterator<V> iterator() {
+        return new Iterator<V>() {
+            private int seek = 0;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                // дальше текущей позиции есть непустые ячейки
+                while (seek < table.length && table[seek] == null) {
+                    seek++;
+                }
+                return seek < table.length;
+            }
+
+            @Override
+            public V next() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table[seek++].value;
+            }
+        };
+    }
+
+
+    public static void main(String[] args) {
+        var sm = new SimpleHashMap<Integer, Integer>();
+        sm.insert(10, 1);
+        sm.insert(11, 2);
+
+        var it = sm.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/collection/map/SimpleHashMapTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.collection.map;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class SimpleHashMapTest {
+
+    @Test
+    public void whenInsertThenGet() {
+        var sm = new SimpleHashMap<String, Integer>();
+        sm.insert("key1", 1);
+        sm.insert("key2", 2);
+        sm.insert("key3", 3);
+        assertThat(sm.get("key3"), is(3));
+        assertThat(sm.get("key2"), is(2));
+    }
+
+    @Test
+    public void whenSameKeysSameHash() {
+        var sm = new SimpleHashMap<Integer, Integer>();
+        assertTrue(sm.insert(1, 10));
+        assertTrue(sm.insert(1, 20));
+        assertThat(sm.get(1), is(20));
+    }
+
+    @Test
+    public void whenDiffKeysSameHash() {
+        var sm = new SimpleHashMap<Integer, Integer>();
+        assertTrue(sm.insert(1, 1));
+        assertFalse(sm.insert(17, 1));
+        assertThat(sm.get(1), is(1));
+    }
+
+    @Test
+    public void whenIterate() {
+        var sm = new SimpleHashMap<String, Integer>();
+        sm.insert("key1", 1);
+        sm.insert("key2", 2);
+        sm.insert("key3", 3);
+        var it = sm.iterator();
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+        assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void whenDelete() {
+        var sm = new SimpleHashMap<String, Integer>();
+        sm.insert("key1", 1);
+        assertThat(sm.get("key1"), is(1));
+        assertTrue(sm.delete("key1"));
+        assertNull(sm.get("key1"));
+        assertFalse(sm.delete("key1"));
+    }
+
+    @Test
+    public void whenResize() {
+        var sm = new SimpleHashMap<Integer, Integer>(4);
+        for (int key = 3; key < 10; key++) {
+            sm.insert(key, key);
+        }
+        assertThat(sm.get(9), is(9));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        var sm = new SimpleHashMap<Integer, Integer>();
+        sm.insert(1, 1);
+        var it = sm.iterator();
+        sm.insert(2, 2);
+        it.next();
+    }
+
+}
