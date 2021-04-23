5fe11666adb1820af9f3763715b1db3a3bd9bd56
serj <sergeykalenikov@gmail.com>
2021-04-23 11:18:45 +0300

    0.1. FileOutputStream. [#513758]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,7 +7,7 @@ public class ResultFile {
         var sb = new StringBuilder();
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
-                sb.append(String.format("%d x %d = %d\n", i, j, i * j));
+                sb.append(String.format("%d x %d = %d%n", i, j, i * j));
             }
         }
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
