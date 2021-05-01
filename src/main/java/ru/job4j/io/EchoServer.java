48a99a29782ee72a7af7fda5ec2f0cc012b500bf
serj <sergeykalenikov@gmail.com>
2021-05-01 12:19:32 +0300

    1. Бот [#513781]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,7 +8,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final String END = "bye";
+    private static final String END = "exit";
+    private static final String HELLO = "hello";
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
@@ -17,14 +18,23 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     String input = in.readLine();
-                    System.out.println(input);
-                    out.write("HTTP/1.1 200 OK\r\n\".getBytes()".getBytes());
-                    if (input.toLowerCase().contains(END)) {
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String response;
+                    if (input.toLowerCase().contains(HELLO)) {
+                        response = "Hello, dear friend";
+                    } else if (input.toLowerCase().contains(END)) {
+                        response = END;
+                    } else {
+                        response = input.toLowerCase().split("=")[1].split("\\s")[0];
+                    }
+                    out.write(response.getBytes());
+                    if (response.equals(END)) {
                         server.close();
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             e.printStackTrace();
         }
     }
