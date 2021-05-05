b1a3ad45b7ca45e452842bc561a22bab461330b1
serj <sergeykalenikov@gmail.com>
2021-05-05 14:16:08 +0300

    4. JAXB. Преобразование XML в POJO. [#513773]

--- src/main/java/ru/job4j/io/serialization/xml/Order.java
+++ src/main/java/ru/job4j/io/serialization/xml/Order.java
@@ -1,14 +1,39 @@
 package ru.job4j.io.serialization.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 
+@XmlRootElement(name = "order")
 public class Order {
+    @XmlAttribute
     private int customerID;
+    @XmlAttribute
     private String comment;
+
+    @XmlElementWrapper(name = "orderLines")
+    @XmlElement(name = "orderLine")
     private List<OrderLine> orderLines;
+
+    @XmlElementWrapper(name = "partyIds")
+    @XmlElement(name = "partyId")
     private String[] partyIds;
+
+    @XmlAttribute
     private boolean isInnerOrder;
+
+    public Order() {
+    }
 
     public Order(int customerID, String comment, List<OrderLine> orderLines, String[] partyIds, boolean isInnerOrder) {
         this.customerID = customerID;
@@ -16,6 +41,36 @@ public class Order {
         this.orderLines = orderLines;
         this.partyIds = partyIds;
         this.isInnerOrder = isInnerOrder;
+    }
+
+    public static void main(String[] args) throws JAXBException {
+
+        final Order order = new Order(10,
+                "ТЕСТОВЫЙ ЗАКАЗ",
+                Arrays.asList(
+                        new OrderLine("PN1", 5),
+                        new OrderLine("PN2", 15)
+                ),
+                new String[]{"ПАРТИЯ1", "ПАРТИЯ50"},
+                true);
+
+        JAXBContext context = JAXBContext.newInstance(Order.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(order, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Order result = (Order) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 
     @Override
@@ -27,10 +82,6 @@ public class Order {
                 ", partyIds=" + Arrays.toString(partyIds) +
                 ", isInnerOrder=" + isInnerOrder +
                 '}';
-    }
-
-    public static void main(String[] args) {
-
     }
 }
 
--- src/main/java/ru/job4j/io/serialization/xml/OrderLine.java
+++ src/main/java/ru/job4j/io/serialization/xml/OrderLine.java
@@ -1,8 +1,18 @@
 package ru.job4j.io.serialization.xml;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+
+import javax.xml.bind.annotation.XmlAttribute;
+
+@XmlElement(value = "orderLine")
 public class OrderLine {
+    @XmlAttribute
     private String productCode;
+    @XmlAttribute
     private int productCount;
+
+    public OrderLine() {
+    }
 
     public OrderLine(String productCode, int productCount) {
         this.productCode = productCode;
