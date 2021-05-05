01f3425ed0247e2be4b4e868e0ea68ff94706e05
serj <sergeykalenikov@gmail.com>
2021-05-05 13:59:06 +0300

    2. Формат JSON [#513771]

--- src/main/java/ru/job4j/io/serialization/json/Order.java
+++ src/main/java/ru/job4j/io/serialization/json/Order.java
@@ -1,4 +1,4 @@
-package ru.job4j.serialization;
+package ru.job4j.io.serialization.json;
 
 import java.util.Arrays;
 import java.util.List;
--- src/main/java/ru/job4j/io/serialization/json/OrderLine.java
+++ src/main/java/ru/job4j/io/serialization/json/OrderLine.java
@@ -1,4 +1,4 @@
-package ru.job4j.serialization;
+package ru.job4j.io.serialization.json;
 
 public class OrderLine {
     private String productCode;
